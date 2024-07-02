767317f5dcada6960f961c19c7073924ac705e0e
ctang <11547113+crtang@users.noreply.github.com>
2024-07-02 12:12:24 -0700

    Made adjustments to dropdown menu UI

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-	var composition = $('#music');
-	var play_button = $('#play-icon');
+	var composition = $('.music');
+	var play_button = $('.play-icon');
 
 	function toggle_audio() {
 		if (!composition.get(0).paused) {
 			composition.get(0).pause();
 			console.log("Paused track");
 			play_button.attr('src', 'images/play icon white.svg');
-			console.log("Changed icon to play icon");
+			/* console.log("Changed icon to play icon"); */
 		} else if (!composition.get(0).started) {
 			composition.get(0).play();
 			console.log("Playing track");
 			play_button.attr('src', 'images/pause icon white.svg');
-			console.log("Changed icon to pause icon");
+			/* console.log("Changed icon to pause icon"); */
 		}
 	}
 
