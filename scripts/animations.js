9999059c4e978723b49ebfabb134996373d45f77
ctang <11547113+crtang@users.noreply.github.com>
2025-06-13 10:06:07 -0700

    Returned to jQuery and fixed play/pause button

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,9 +1,25 @@
-$(function() {
+$(document).ready(function() {
 	let composition = $('.music');
+	let audio = composition.get(0);
+
+	let playButton = $('.play-icon');
 	let elapsed = $('.currrent');
 	let time = $('.length');
 
-	composition.loop = false;
+	// toggle play/pause button
+	function toggleAudio() {
+		if (!audio.paused) {
+			audio.pause();
+			playButton.attr('src', 'images/play%20icon%20white.svg');
+		} else if (audio.paused) {
+			audio.play();
+			playButton.attr('src', 'images/pause%20icon%20white.svg');
+		}
+	};
+
+	playButton.click(function() {
+		toggleAudio();
+	});
 });
 
 
