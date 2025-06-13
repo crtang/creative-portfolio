3ea445a99752f44b91df6cccc71ab4ff028d7b67
ctang <11547113+crtang@users.noreply.github.com>
2025-06-13 09:22:00 -0700

    Updated jQuery file and related links

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,9 +1,9 @@
-if (document.readyState === 'complete') {
-	let composition = document.querySelector('.music');
-	let audio = composition.get(0);
-	let play_button = document.querySelector('.play-icon');
-	
-	audio.loop = false;
-}
+$(function() {
+	let composition = $('.music');
+	let elapsed = $('.currrent');
+	let time = $('.length');
+
+	composition.loop = false;
+});
 
 
