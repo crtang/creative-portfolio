9552486e89899e58c02dafd3604468d74794663b
ctang <11547113+crtang@users.noreply.github.com>
2025-06-13 10:19:26 -0700

    Fixed volume on/off button and also changed alt text to match button icon changes

--- scripts/animations.js
+++ scripts/animations.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 	let audio = composition.get(0);
 
 	let playButton = $('.play-icon');
+	let volumeButton = $('.volume-icon');
+
 	let elapsed = $('.currrent');
 	let time = $('.length');
 
@@ -10,15 +12,29 @@ $(document).ready(function() {
 	function toggleAudio() {
 		if (!audio.paused) {
 			audio.pause();
-			playButton.attr('src', 'images/play%20icon%20white.svg');
+			playButton.attr({'src':'images/play%20icon%20white.svg', 'alt':'play icon'});
 		} else if (audio.paused) {
 			audio.play();
-			playButton.attr('src', 'images/pause%20icon%20white.svg');
+			playButton.attr({'src':'images/pause%20icon%20white.svg', 'alt':'pause icon'});
+		}
+	};
+
+	// toggle volume on/off
+	function toggleVolume() {
+		if (!audio.muted) {
+			audio.muted = true;
+			volumeButton.attr({'src':'images/volume-up.svg', 'alt':'volume on icon'});
+		} else {
+			audio.muted = false;
+			volumeButton.attr({'src':'images/volume-mute.svg', 'alt':'volume off icon'});
 		}
 	};
 
 	playButton.click(function() {
 		toggleAudio();
+	});
+	volumeButton.click(function() {
+		toggleVolume();
 	});
 });
 
